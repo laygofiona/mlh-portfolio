91a396c0778475ad17e414a2696a28ae65a377b4
Pablo Armando Mac Beath Milian <pabloarmando.macbeath@gmail.com>
2025-06-21 18:29:41 -0600

    Education template with jinja

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,60 @@ def hobbies():
 def about():
     return render_template('aboutUs.html', title="About Us", url=os.getenv("URL"))
 
+#Route for education page
+@app.route('/education')
+def education():
+    # Create a dictionary with education data for each person
+    education_data = {
+        "Fiona": [
+            {
+                "degree": "Primary School Diploma",
+                "institution": "Andes International School",
+                "dates": "2010 - 2016",
+                "description": "School with an international curriculum.",
+                "icon": "fas fa-school"
+            },
+            {
+                "degree": "Middle School Diploma",
+                "institution": "Andes International School",
+                "dates": "2016 - 2019",
+                "description": "Graduated with honors. ",
+                "icon": "fas fa-school"
+            },
+        ],
+        "Armando": [
+            {
+                "degree": "Primary School Diploma",
+                "institution": "Andes International School",
+                "dates": "2010 - 2016",
+                "description": "School with an international curriculum.",
+                "icon": "fas fa-school"
+            },
+            {
+                "degree": "Middle School Diploma",
+                "institution": "Andes International School",
+                "dates": "2016 - 2019",
+                "description": "Graduated with honors. ",
+                "icon": "fas fa-school"
+            },
+            {
+                "degree": "High School Diploma",
+                "institution": "Prepa Tec Campus Puebla",
+                "dates": "2018 - 2021",
+                "description": "Program of international baccalaureate.",
+                "icon": "fas fa-school"
+            },
+            {
+                "degree": "B.S. in Mechatronics Engineering",
+                "institution": "Tecnológico de Monterrey, Campus Puebla",
+                "dates": "2021 - 2025",
+                "description": "Specializing in automation and robotics. Lead programmer for the FRC team.",
+                "icon": "fas fa-graduation-cap"
+            }
+        ]
+    }
+    # Render the education template with the education data
+    return render_template('education.html', education_data=education_data)
 
 # map route which returns the map html page
 @app.route('/map')
