df543addffdffbcf0926eee52aff2392dd415236
laygofiona <laygo.fiona@gmail.com>
2025-07-10 15:18:35 -0400

    Adding endpoints and database to flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,34 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
+from playhouse.shortcuts import model_to_dict
+import datetime
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+                     user=os.getenv("MYSQL_USER"),
+                     password=os.getenv("MYSQL_PASSWORD"),
+                     host=os.getenv("MYSQL_HOST"),
+                     port=3306)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+    
+    class Meta:
+        database = mydb
+        
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+print(mydb)
+
+
 
 
 @app.route('/')
@@ -92,3 +117,26 @@ def education():
 def map():
     return render_template('map.html', title="Places we've been", url=os.getenv("URL"))
 
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in 
+            TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+@app.route('/api/timeline_post', methods=['DELETE'])
+def delete_time_line_posts():
+    query = TimelinePost.delete()
+    query.execute()
+    return f"Posts deleted successfully", 200
