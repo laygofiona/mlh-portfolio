1469b1439d9689587d09430ee86daabda4aa46d1
laygofiona <laygo.fiona@gmail.com>
2025-07-25 19:43:02 +0000

    Updating redeploy site script to spin up docker container

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+MY_CONTAINER='my_portfolio'
 
+# make sure script stops when a command fails
+set -eu
 
+# move into project folder
 cd mlh-portfolio
 
+echo "Fetching latest changes from remote repo and updating current working directory"
+# fetch the latest changes from remote repo on github and set current local directory to match changes from branch
 git fetch && git reset origin/main --hard
 
-source python3-virtualenv/bin/activate
+# check if production container is running before stopping it
+echo "Checking for any running production containers"
+if [$(docker ps | grep -ic "$MY_CONTAINER") -gt 0]; then
+	echo "Stopping running container: $MY_CONTAINER"
+	# there is a running container so stop it
+	docker stop $MY_CONTAINER
+fi
 
-pip install -r requirements.txt
+echo "Building portfolio production image and starting its container"
+# build docker image in docker-compose.prod.yml and start its container
+docker compose -f docker-compose.prod.yml up -d --build
 
-systemctl daemon-reload
 
-systemctl restart myportfolio
